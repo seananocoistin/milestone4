b540412a907eb0395145a738804bdc45cef5e114
Seanán Ó Coistín <seananoc@gmail.com>
2020-09-06 14:50:25 +0000

    Added forms to the checkout app

--- premiumbd/settings.py
+++ premiumbd/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'phone_field',
     'cart',
     'checkout',
+
+    # OTHER
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'premiumbd.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -63,8 +68,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field'
+            ]
         },
     },
 ]
