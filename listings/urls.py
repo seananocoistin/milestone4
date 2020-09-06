35a624be068f5227dfca62f12ebc648c30661e14
Seanán Ó Coistín <seananoc@gmail.com>
2020-09-06 21:12:50 +0000

    Add urls and a new view.py

--- listings/urls.py
+++ listings/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from . import views
-
 urlpatterns = [
-    path('', views.all_listings, name='listings'),
-    path('<listing_id>', views.listing_detail, name='listing_detail'),
+    path('', views.all_listings, name='listing'),
+    path('<int:listing_id>/', views.listing_detail, name='listing_detail'),
+    path('add/', views.add_listing, name='add_listing'),
+    path('edit/<int:listing_id>/', views.edit_listing, name='edit_listing'),
+    path('delete/<int:listing_id>/', views.delete_listing, name='delete_listing'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -4,11 +4,12 @@ from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Listing, Category
+from .forms import ListingForm
 
 # Create your views here.
 
-
 def all_listings(request):
+    """ A view to show all listings, including sorting and search queries """
 
     listings = Listing.objects.all()
     query = None
@@ -24,27 +25,25 @@ def all_listings(request):
                 sortkey = 'lower_name'
                 listings = listings.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
-                sortkey = 'category_name'
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             listings = listings.order_by(sortkey)
-
-    if request.GET:
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             listings = listings.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "No search words were entered")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('listings'))
             
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(about__icontains=query)
             listings = listings.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
@@ -55,16 +54,70 @@ def all_listings(request):
         'current_categories': categories,
         'current_sorting': current_sorting,
     }
-    
-    return render(request, 'listings/listings.html')
+
+    return render(request, 'listings/listings.html', context)
 
 
 def listing_detail(request, listing_id):
+    """ A view to show individual listing details """
 
     listing = get_object_or_404(Listing, pk=listing_id)
 
     context = {
         'listing': listing,
     }
-    
+
     return render(request, 'listings/listing_detail.html', context)
+
+
+def add_listing(request):
+    """ Add a listing to the store """
+    if request.method == 'POST':
+        form = ListingForm(request.POST, request.FILES)
+        if form.is_valid():
+            listing = form.save()
+            messages.success(request, 'Successfully added listing!')
+            return redirect(reverse('listing_detail', args=[listing.id]))
+        else:
+            messages.error(request, 'Failed to add listing. Please ensure the form is valid.')
+    else:
+        form = ListingForm()
+        
+    template = 'listings/add_listing.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_listing(request, listing_id):
+    """ Edit a listing in the store """
+    listing = get_object_or_404(Listing, pk=listing_id)
+    if request.method == 'POST':
+        form = ListingForm(request.POST, request.FILES, instance=listing)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated listing!')
+            return redirect(reverse('listing_detail', args=[listing.id]))
+        else:
+            messages.error(request, 'Failed to update listing. Please ensure the form is valid.')
+    else:
+        form = ListingForm(instance=listing)
+        messages.info(request, f'You are editing {listing.name}')
+
+    template = 'listings/edit_listing.html'
+    context = {
+        'form': form,
+        'listing': listing,
+    }
+
+    return render(request, template, context)
+
+
+def delete_listing(request, listing_id):
+    """ Delete a listing from the store """
+    listing = get_object_or_404(Listing, pk=listing_id)
+    listing.delete()
+    messages.success(request, 'Listing deleted!')
+    return redirect(reverse('listings'))
