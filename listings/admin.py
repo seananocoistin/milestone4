22bef9234eb27db6c85e3beb558c893690959ed7
Seanán Ó Coistín <seananoc@gmail.com>
2020-09-06 11:44:31 +0000

    Customised the admin

--- listings/admin.py
+++ listings/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import Listing, Category
 
 # Register your models here.
-admin.site.register(Listing)
-admin.site.register(Category)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'address',
+        'category',
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Category, CategoryAdmin)
