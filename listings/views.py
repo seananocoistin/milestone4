8a6ae1831f4eb8771b0ec97048ebd4d535cad142
Seanán Ó Coistín <seananoc@gmail.com>
2020-09-06 12:28:21 +0000

    Added category sorting

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Listing
+from .models import Listing, Category
 
 # Create your views here.
 
@@ -9,6 +9,13 @@ def all_listings(request):
 
     listings = Listing.objects.all()
     query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            listings = listings.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     if request.GET:
         if 'q' in request.GET:
@@ -23,6 +30,7 @@ def all_listings(request):
     context = {
         'listings': listings,
         'search_term': query,
+        'current_categories': categories,
     }
     
     return render(request, 'listings/listings.html')
