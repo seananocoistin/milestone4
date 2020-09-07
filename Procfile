b145993cf180fcaeb50cd0d5f88f0d849ec31535
Seanán Ó Coistín <seananoc@gmail.com>
2020-09-07 15:38:27 +0000

    Configured the Heroku app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn premiumbdhe.wsgi:application
--- premiumbd/settings.py
+++ premiumbd/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = '(9%ae(1l1mm7v=tv8j$v+qcw%geq_s$5%#q5pf8oxf)%)s#m*f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['premiumbd.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -109,12 +109,18 @@ WSGI_APPLICATION = 'premiumbd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
